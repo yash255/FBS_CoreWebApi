990b43c1764c86fd1e218ed3e57bc02fbe824a13
Yash Khandelwal <yash31khandelwal@gmail.com>
2023-04-27 18:36:22 +0530

    commit 6

--- FBS_CoreApi/Program.cs
+++ FBS_CoreApi/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Flight Booking System", Version = "v1" });
 
     // Add a security definition for JWT bearer tokens
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
